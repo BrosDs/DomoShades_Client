e1febe5c9d5ad116fda2e9d5d87c48670521cf36
Dario <brosds@gmail.com>
2015-03-23 16:17:50 +0100

    Finished first wrapper

--- src/it/unimore/awd/DomoWrapper.java
+++ src/it/unimore/awd/DomoWrapper.java
@@ -1,7 +1,6 @@
 package it.unimore.awd;
 
-import Classes.Home;
-import Classes.User;
+import Classes.*;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.restlet.data.MediaType;
@@ -20,6 +19,8 @@ public class DomoWrapper {
         this.scope="";
     }
 
+
+    /** User Functions **/
     public User getUser(String email) throws IOException {
         this.scope="/user?email="+email;
         ClientResource cr = new ClientResource(uri+scope);
@@ -27,8 +28,7 @@ public class DomoWrapper {
         if(returnString==null)
             return null;
         Gson gson = new Gson();
-        User user = gson.fromJson(returnString,User.class);
-        return user;
+        return gson.fromJson(returnString,User.class);
     }
 
     public User putUser(String email, String first_name, String last_name, String profile_pic) throws IOException {
@@ -38,8 +38,7 @@ public class DomoWrapper {
         if(returnString==null)
             return null;
         Gson gson = new Gson();
-        User user = gson.fromJson(returnString,User.class);
-        return user;
+        return gson.fromJson(returnString,User.class);
     }
 
     public User deleteUser(String email) throws IOException {
@@ -49,11 +48,12 @@ public class DomoWrapper {
         if(returnString==null)
             return null;
         Gson gson = new Gson();
-        User user = gson.fromJson(returnString,User.class);
-        return user;
+        return gson.fromJson(returnString,User.class);
     }
 
-    public List<Home> getHomeList(String owner) throws IOException {
+    /** Home functions **/
+
+    public List<Home> getHomesByUser(String owner) throws IOException {
         this.scope="/home?owner="+owner;
         ClientResource cr = new ClientResource(uri+scope);
         String returnString = cr.get(MediaType.APPLICATION_JSON).getText();
@@ -61,8 +61,7 @@ public class DomoWrapper {
             return null;
         Gson gson = new Gson();
         TypeToken<List<Home>> token = new TypeToken<List<Home>>(){};
-        List<Home> home = gson.fromJson(returnString,token.getType());
-        return home;
+        return gson.fromJson(returnString,token.getType());
     }
 
     public Home putHome(String owner, String description, String city, int cap, String country, String address) throws IOException {
@@ -73,14 +72,9 @@ public class DomoWrapper {
         if(returnString.equals("[]"))
             return null;
         Gson gson = new Gson();
-        Home home = gson.fromJson(returnString,Home.class);
-        return home;
+        return gson.fromJson(returnString,Home.class);
     }
 
-    /**
-     * /home? owner=owner_email_address&
-     *        id=home_id_number
-     */
     public List<Home> deleteHome(String owner, String id) throws IOException {
         this.scope="/home?owner="+owner+"&id="+id;
         ClientResource cr = new ClientResource(uri+scope);
@@ -89,7 +83,112 @@ public class DomoWrapper {
             return null;
         Gson gson = new Gson();
         TypeToken<List<Home>> token = new TypeToken<List<Home>>(){};
-        List<Home> home = gson.fromJson(returnString,token.getType());
-        return home;
+        return gson.fromJson(returnString,token.getType());
+    }
+
+    /** Floor Functions **/
+
+    public List<Floor> getFloorsByHome(String owner, String home) throws IOException {
+        this.scope=String.format("/floor?owner=%s&home=%s", owner,home);
+        ClientResource cr = new ClientResource(uri+scope);
+        String returnString = cr.get(MediaType.APPLICATION_JSON).getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        TypeToken<List<Floor>> token = new TypeToken<List<Floor>>(){};
+        return gson.fromJson(returnString,token.getType());
+    }
+
+
+    public Floor putFloor(String owner, String home, String id, String type, String canvas) throws IOException {
+        this.scope=String.format("/floor?owner=%s&home=%s&id=%s&type=%s&canvas=%s", owner,home,id,type,canvas);
+        ClientResource cr = new ClientResource(uri+scope);
+        System.out.println(uri+scope);
+        String returnString = cr.put(Floor.class).getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        return gson.fromJson(returnString,Floor.class);
+    }
+
+    public List<Floor> deleteFloor(String owner, String home, String id) throws IOException {
+        this.scope=String.format("/floor?owner=%s&home=%s&id=%s", owner,home,id);
+        ClientResource cr = new ClientResource(uri+scope);
+        System.out.println(uri+scope);
+        String returnString = cr.delete().getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        TypeToken<List<Floor>> token = new TypeToken<List<Floor>>(){};
+        return gson.fromJson(returnString,token.getType());
+    }
+
+    /** Room functions **/
+
+    public List<Room> getRoomsByFloor(String owner, String home, String id) throws IOException {
+        this.scope=String.format("/room?owner=%s&home=%s&id=%s", owner,home,id);
+        ClientResource cr = new ClientResource(uri+scope);
+        String returnString = cr.get(MediaType.APPLICATION_JSON).getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        TypeToken<List<Room>> token = new TypeToken<List<Room>>(){};
+        return gson.fromJson(returnString,token.getType());
+    }
+
+    public Room putRoom(String owner, String home, String id, String room_id, String name) throws IOException {
+        this.scope=String.format("/room?owner=%s&home=%s&id=%s&room_id=%s&name=%s", owner,home,id,room_id,name);
+        ClientResource cr = new ClientResource(uri+scope);
+        String returnString = cr.put(Room.class).getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        return gson.fromJson(returnString,Room.class);
+    }
+
+    public List<Room> deleteRoom(String owner, String home, String id, String room_id) throws IOException {
+        this.scope=String.format("/room?owner=%s&home=%s&id=%s&room_id=%s", owner,home,id,room_id);
+        ClientResource cr = new ClientResource(uri+scope);
+        String returnString = cr.delete().getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        TypeToken<List<Room>> token = new TypeToken<List<Room>>(){};
+        return gson.fromJson(returnString,token.getType());
+    }
+
+    /** Window functions **/
+
+    public List<Window> getWindowsOfRoom(String owner, String home, String id, String room_id) throws IOException {
+        this.scope=String.format("/window?owner=%s&home=%s&id=%s&room_id=%s", owner,home,id,room_id);
+        ClientResource cr = new ClientResource(uri+scope);
+        String returnString = cr.get(MediaType.APPLICATION_JSON).getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        TypeToken<List<Window>> token = new TypeToken<List<Window>>(){};
+        return gson.fromJson(returnString,token.getType());
+    }
+
+
+    public Window putWindow(String owner, String home, String id, String room_id, String window_id) throws IOException {
+        this.scope=String.format("/window?owner=%s&home=%s&id=%s&room_id=%s&window_id=%s", owner,home,id,room_id,window_id);
+        ClientResource cr = new ClientResource(uri+scope);
+        String returnString = cr.put(Window.class).getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        return gson.fromJson(returnString,Window.class);
+    }
+
+    public List<Window> deleteWindow(String owner, String home, String id, String room_id, String window_id) throws IOException {
+        this.scope=String.format("/window?owner=%s&home=%s&id=%s&room_id=%s&window_id=%s", owner,home,id,room_id,window_id);
+        ClientResource cr = new ClientResource(uri+scope);
+        String returnString = cr.delete().getText();
+        if(returnString.equals("[]"))
+            return null;
+        Gson gson = new Gson();
+        TypeToken<List<Window>> token = new TypeToken<List<Window>>(){};
+        return gson.fromJson(returnString,token.getType());
     }
 }
--- src/it/unimore/awd/TestClass.java
+++ src/it/unimore/awd/TestClass.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 public class TestClass {
     public static void main(String[] args) throws IOException {
         DomoWrapper domoWrapper = new DomoWrapper();
+        String owner = "brosds@gmail.com";
+        String home = "5741031244955648";
+        String id = "2";
+        String room_id = "3";
 
         /** User usage example
         // GET
@@ -18,7 +22,7 @@ public class TestClass {
         System.out.println(domoWrapper.putUser("brosds@gmail.com","Dario","Stabili","http://dummy.pic/ture"));
 
         // DELETE
-        System.out.println(domoWrapper.deleteUser("brosds@gmail.com"));
+         System.out.println(domoWrapper.deleteUser("brosds@gmail.com"));
 
         */
 
@@ -30,9 +34,9 @@ public class TestClass {
          // GET
         List<Home> hl = domoWrapper.getHomeList("brosds@gmail.com");
         if(hl==null)
-            System.out.println("No Home retrieved");
-        else
-            System.out.println(hl.size());
+         System.out.println("No Home retrieved");
+         else
+         System.out.println(hl.size());
 
          //DELETE
         hl = domoWrapper.deleteHome("brosds@gmail.com", "5668600916475904");
@@ -44,7 +48,69 @@ public class TestClass {
          */
 
 
+        /** Floor Usage example
+
+        // PUT
+         System.out.println(domoWrapper.putFloor(owner,home,"2","0","piano terra"));
 
 
+
+        //GET
+        List<Floor> lf = domoWrapper.getFloorsByHome(owner,home);
+        if(lf==null)
+            System.out.println("No Floor in this home");
+        else
+            for(Floor f : lf)
+                System.out.println(f);
+
+
+        // DELETE
+        lf = domoWrapper.deleteFloor(owner,home,id);
+
+        if(lf==null)
+            System.out.println("No Floor in this home");
+        else
+            for(Floor f : lf)
+                System.out.println(f);
+        */
+
+        /** Room usage example
+         System.out.println(domoWrapper.putRoom(owner,home,id,"1","Bagno"));
+         System.out.println(domoWrapper.putRoom(owner,home,id,"2","Cucina"));
+         System.out.println(domoWrapper.putRoom(owner,home,id,"3","Sala"));
+         System.out.println(domoWrapper.putRoom(owner,home,id,"4","Terrazzo"));
+
+         List<Room> lr = domoWrapper.getRoomsByFloor(owner,home,id);
+         for(Room r : lr){
+         System.out.println(r);
+         }
+
+         lr = domoWrapper.deleteRoom(owner,home,id,"4");
+
+         for(Room r : lr){
+         System.out.println(r);
+         }
+
+         **/
+
+        /** Window usage example
+        System.out.println(domoWrapper.putWindow(owner,home,id,room_id,"1"));
+        System.out.println(domoWrapper.putWindow(owner,home,id,room_id,"2"));
+        System.out.println(domoWrapper.putWindow(owner,home,id,room_id,"3"));
+        System.out.println(domoWrapper.putWindow(owner,home,id,room_id,"4"));
+
+
+
+        List<Window> lr = domoWrapper.getWindowsOfRoom(owner, home, id, room_id);
+        for(Window r : lr){
+            System.out.println(r);
+        }
+
+        lr = domoWrapper.deleteWindow(owner, home, id, room_id, "4");
+
+        for(Window r : lr){
+            System.out.println(r);
+        }
+        **/
     }
 }
